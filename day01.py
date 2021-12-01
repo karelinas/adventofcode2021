7c4f8aa2b8450ab139096701169672eec68efaf4
karelinas <45606302+karelinas@users.noreply.github.com>
2021-12-01 22:59:12 +0200

    Day 01: optimize solution
    The sliding windows share two data points, so we only have to compare
    the item that is different. That is, `sum([1, 2, 3]) < sum([2, 3, 4])`
    simplifies to just `1 < 4`.

--- day01.py
+++ day01.py
@@ -1,17 +1,8 @@
 from sys import stdin
 
-
-def windows(lst, n=3):
-    while len(lst) >= n:
-        yield lst[:n]
-        lst = lst[1:]
-
-
 depths = list(map(int, stdin))
-increasecount = sum(a < b for a, b in zip(depths[:-1], depths[1:]))
-windowedcount = sum(
-    sum(a) < sum(b) for a, b in zip(windows(depths[:-1]), windows(depths[1:]))
-)
+increasecount = sum(a < b for a, b in zip(depths, depths[1:]))
+windowedcount = sum(a < d for a, d in zip(depths, depths[3:]))
 
 print("Increase count:", increasecount)
 print("Windowed count:", windowedcount)
