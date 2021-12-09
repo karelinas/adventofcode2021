a3b45c6b3287323ff272f3edd7cb4fb187db822d
karelinas <45606302+karelinas@users.noreply.github.com>
2021-12-09 20:16:09 +0200

    Day 09: solution

---
+++ day09.py
@@ -0,0 +1,45 @@
+from sys import stdin
+from math import prod
+
+
+def is_in_grid(grid, x, y):
+    return 0 <= y < len(grid) and 0 <= x < len(grid[y])
+
+
+def neighbors(x, y):
+    return [
+        (x, y - 1),
+        (x - 1, y),
+        (x + 1, y),
+        (x, y + 1),
+    ]
+
+
+def is_low_point(grid, x, y):
+    return all(
+        grid[y][x] < grid[j][i] for i, j in neighbors(x, y) if is_in_grid(grid, i, j)
+    )
+
+
+def basin_size(grid, x, y, visited=None):
+    if not visited:
+        visited = set()
+    if grid[y][x] == 9 or (x, y) in visited:
+        return 0
+    visited.add((x, y))
+    return 1 + sum(
+        basin_size(grid, i, j, visited)
+        for i, j in neighbors(x, y)
+        if is_in_grid(grid, i, j)
+    )
+
+
+grid = [[int(x) for x in row] for row in stdin.read().split()]
+low_points = [
+    (x, y)
+    for y, row in enumerate(grid)
+    for x, val in enumerate(row)
+    if is_low_point(grid, x, y)
+]
+print(sum(grid[y][x] + 1 for x, y in low_points))
+print(prod(sorted(basin_size(grid, x, y) for x, y in low_points)[-3:]))
