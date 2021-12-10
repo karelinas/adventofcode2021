aae5be4b3e77c1ea03ff5d6c8cf865af00b9e125
karelinas <45606302+karelinas@users.noreply.github.com>
2021-12-10 19:00:42 +0200

    Day 10: refactor common code into function

--- day10.py
+++ day10.py
@@ -3,32 +3,29 @@ from statistics import median
 from sys import stdin
 
 
-OPENERS = ["(", "[", "{", "<"]
-CLOSERS = [")", "]", "}", ">"]
+def parse(line):
+    openers = ["(", "[", "{", "<"]
+    closers = [")", "]", "}", ">"]
+    expected_openers = {closer: opener for closer, opener in zip(closers, openers)}
+    stack = []
+    for ch in line:
+        if ch in openers:
+            stack.append(ch)
+        elif stack.pop() != expected_openers[ch]:
+            return stack, ch
+    return stack, None
 
 
 def syntax_checker_score(line):
     point_values = {")": 3, "]": 57, "}": 1197, ">": 25137}
-    expected_openers = {closer: opener for closer, opener in zip(CLOSERS, OPENERS)}
-    stack = []
-    for ch in line:
-        if ch in OPENERS:
-            stack.append(ch)
-        elif stack.pop() != expected_openers[ch]:
-            return point_values[ch]
-    return 0
+    _, unexpected = parse(line)
+    return point_values[unexpected] if unexpected else 0
 
 
 def autocomplete_score(line):
-    stack = []
-    for ch in line:
-        if ch in OPENERS:
-            stack.append(ch)
-        else:
-            stack.pop()
-
     point_values = {"(": 1, "[": 2, "{": 3, "<": 4}
-    return reduce(lambda x, y: x * 5 + point_values[y], reversed(stack), 0)
+    leftovers, _ = parse(line)
+    return reduce(lambda x, y: x * 5 + point_values[y], reversed(leftovers), 0)
 
 
 lines = [line.strip() for line in stdin]
