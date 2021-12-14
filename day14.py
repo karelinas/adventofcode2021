14cd58719c85011a1c20f3bf97c399b601489baf
karelinas <45606302+karelinas@users.noreply.github.com>
2021-12-14 23:29:49 +0200

    Day 14: simplify insertion()

--- day14.py
+++ day14.py
@@ -1,6 +1,5 @@
 from collections import Counter
 from dataclasses import dataclass
-from itertools import chain
 from sys import stdin
 from typing import Dict
 
@@ -23,14 +22,13 @@ class Polymer:
 
 def insertion(polymer: Polymer) -> Polymer:
     new_polymer = polymer.copy()
-    for pair, count in polymer.pair_counts.items():
-        if pair in rules:
-            new_element = rules[pair]
-            left, right = iter(pair)
-            new_polymer.element_counts[new_element] += count
-            new_polymer.pair_counts[left + new_element] += count
-            new_polymer.pair_counts[new_element + right] += count
-            new_polymer.pair_counts[pair] -= count
+    for pair, new_element in rules.items():
+        left, right = iter(pair)
+        count = polymer.pair_counts[pair]
+        new_polymer.element_counts[new_element] += count
+        new_polymer.pair_counts[left + new_element] += count
+        new_polymer.pair_counts[new_element + right] += count
+        new_polymer.pair_counts[pair] -= count
     return new_polymer
 
 
@@ -38,9 +36,7 @@ template = list(next(stdin).strip())
 rules = dict(rule.strip().split(" -> ") for rule in stdin if rule.strip())
 
 polymer_0 = Polymer(
-    pair_counts=Counter(
-        left + right for left, right in zip(template, template[1:])
-    ),
+    pair_counts=Counter(left + right for left, right in zip(template, template[1:])),
     element_counts=Counter(iter(template)),
     rules=rules,
 )
